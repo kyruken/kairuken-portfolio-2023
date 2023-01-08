ac6a9734cab3a7851d9c1a222a6bb1e0d54acded
Avery Kyle Arenas <kyruken@gmail.com>
2023-01-08 10:14:32 -0800

    add hero and navbar styles

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,16 +1,16 @@
 
 export default function Hero() {
     return (
-        <header>
-            <h1>Hi, I'm Kyle.</h1>
-            <h2>I'm a full stack developer.</h2>
-            <div>
-                <button>Projects</button>
-                <button>About me</button>
+        <header className='flex-column ai-center jc-center padding-tb-4'>
+            <h1 className="f-weight-light">Hi, I'm <span className='highlight'>Kyle.</span></h1>
+            <h2 className="f-weight-light">I'm a <span className="highlight">full stack developer.</span></h2>
+            <div className="padding-tb-0 flex gap-3">
+                <button className="red-button">Projects</button>
+                <button className="greenline-button">About me</button>
             </div>
-            <div>L</div>
-            <div>G</div>
-            <div>E</div>
+            <div className='side-button side-button-l'>L</div>
+            <div className='side-button side-button-g'>G</div>
+            <div className='side-button side-button-e'>E</div>
         </header>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 
 export default function Navbar() {
     return (
-        <nav className='flex jc-spacebetween padding-lr-1'>
+        <nav className='flex jc-spacebetween ai-center padding-lr-1'>
             <h2>Kyle Arenas</h2>
             <ul className='flex gap-2'>
                 <li>Home</li>

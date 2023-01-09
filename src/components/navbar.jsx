1db71d059123a481084372495b01cd22b412a9e9
Avery Kyle Arenas <kyruken@gmail.com>
2023-01-08 18:05:19 -0800

    add hamburger menu, add responsiveness to hero&nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 
 export default function Navbar() {
     return (
-        <nav className='flex jc-spacebetween ai-center padding-lr-1'>
+        <nav className='flex jc-spacebetween ai-center padding-lr-3'>
             <h2>Kyle Arenas</h2>
             <ul className='flex gap-2'>
                 <li>Home</li>
@@ -9,6 +9,11 @@ export default function Navbar() {
                 <li>Skills</li>
                 <li>Projects</li>
             </ul>
+            <div className='hamburger-menu'>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
         </nav>
     )
 }

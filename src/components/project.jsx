bb071de6d3ac10f9ff4bf46c3a412b9c344b84e1
Avery Kyle Arenas <kyruken@gmail.com>
2023-01-10 16:57:07 -0800

    project section text styled

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,18 +1,20 @@
 
 export default function Project() {
     return (
-        <article className="content-container ai-center">
-            <div className="flex-column">
+        <article className="content-container ai-center margin-bottom-3">
+            <div className="flex-column gap-3">
                 <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <div className="flex jc-center">
-                    <button>Demo</button>
-                    <button>Code</button>
+                <div className="paragraph-container">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="flex jc-center gap-4">
+                    <button className="red-button">Demo</button>
+                    <button className="red-button">Code</button>
                 </div>
             </div>
             <img alt='img about project'></img>
--- src/components/reverseproject.jsx
+++ src/components/reverseproject.jsx
@@ -1,19 +1,21 @@
 
 export default function ReverseProject() {
     return (
-        <article className="content-container content-reverse ai-center">
+        <article className="content-container content-reverse ai-center margin-bottom-3">
             <img alt='img about project'></img>
-            <div className="flex-column">
+            <div className="flex-column gap-3">
                 <h3>Project Title</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <div className="flex jc-center">
-                    <button>Demo</button>
-                    <button>Code</button>
+                <div className="paragraph-container">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="flex jc-center gap-4">
+                    <button className="red-button">Demo</button>
+                    <button className="red-button">Code</button>
                 </div>
             </div>
         </article>

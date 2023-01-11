8100fdc1efc65c4018a62801095bd1598ef32d2d
Avery Kyle Arenas <kyruken@gmail.com>
2023-01-11 12:38:41 -0800

    add icons to skills, style socials

--- src/components/otherprojects.jsx
+++ src/components/otherprojects.jsx
@@ -1,7 +1,7 @@
 import MiniProject from "./miniprojects"
 export default function OtherProjects() {
     return (
-        <section>
+        <section className="bg-secondary">
             <h2>Other Projects</h2>
             <div className="otherprojects-container padding-1">
                 <MiniProject />
--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -4,18 +4,18 @@ export default function Socials() {
         <section>
             <h2>Socials/Contact</h2>
             <div className='flex content-reverse ai-center jc-center gap-5'>
-                <div className="flex-column ai-center jc-flexend contact-container">
-                    <img alt='linkedin icon'></img>
+                <a href='/' className="flex-column ai-center jc-flexend contact-container padding-tb-0">
+                    <img className='margin-left-1' src='../src/assets/linkedin.png' alt='linkedin icon' width='175'></img>
                     <h3>Linkedin</h3>
-                </div>
-                <div className="flex-column ai-center jc-flexend contact-container">
-                    <img alt='github icon'></img>
+                </a>
+                <a href='/' className="flex-column ai-center jc-flexend contact-container padding-tb-0">
+                    <img src='../src/assets/github.png' alt='github icon' width='150'></img>
                     <h3>Github</h3>
-                </div>
-                <div className="flex-column ai-center jc-flexend contact-container">
-                    <img alt='email icon'></img>
+                </a>
+                <a href='/' className="flex-column ai-center jc-flexend contact-container padding-tb-0">
+                    <img src='../src/assets/email.png' alt='email icon' width='150'></img>
                     <h3>Email</h3>
-                </div>
+                </a>
             </div>
         </section>
     )
